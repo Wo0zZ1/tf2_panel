d6b592cf00389e0f6ec33c57fbe5458ae3a1f03e
Wo0zZ1 <Mper06@mail.ru>
2023-12-08 16:17:14 +0300

    hot fix

--- start.bat
+++ start.bat
@@ -1,9 +1,4 @@
 @echo off
-for /f "usebackq tokens=1,* delims==" %%a in ("C:/Users/Wo0zZ1/Desktop/panel/config.cfg") do (
-    if "%%a"=="PANELPATH" (
-        set "data=%%b"
-    )
-)
-cd /d %data%
+cd /d %~dp0
 python main.py
 pause
